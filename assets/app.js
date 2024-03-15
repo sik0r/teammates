9df0e0d784dddb7495d8a41c861e42902c82699f
Dawid Sikorski <dsikorski40@gmail.com>
2024-03-15 13:21:19 +0100

    add base html template with navbar

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,11 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
